0b7ff6d1d455c0126114a0716bdf4acddecd4f4d
lingz <zhangling.me@foxmail.com>
2021-10-15 20:32:50 +0800

    提取env和config

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // token
+  Key: '',
+  // 这个列表填入你要助力的好友的shareCode,最多5个
+  shareCodes: [
+    'a6f686a9f6aa4c80977370b03681c553',
+    'f92cb56c6a1349f5a35f0372aa041ea0',
+    '6fbd26cc27ac44d6a7fed34092453f77',
+    '61ff5c624949454aa88561f2cd721bf6',
+  ]
+}
